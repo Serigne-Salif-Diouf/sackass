ac53550f13f29b6f4f35ec636c852c34d21f02f1
Serigne-Salif-Diouf <serignesalioudiouf524@gmail.com>
2024-01-12 18:38:33 +0000

    Sackass ionic

---
+++ src/app/activity.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActivityService {
+
+  constructor() { }
+
+   getActivity () { }
+
+   getActivities () { }
+}
